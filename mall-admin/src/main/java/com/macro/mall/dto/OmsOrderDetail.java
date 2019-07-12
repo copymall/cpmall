755ceba15d24cca3393904e4f2e6233970bd1394
limingfei <limingfei@wondertek.com.cn>
2019-07-12 13:27:13 +0800

    添加订单接口

---
+++ mall-admin/src/main/java/com/macro/mall/dto/OmsOrderDetail.java
@@ -0,0 +1,23 @@
+package com.macro.mall.dto;
+
+import com.macro.mall.model.OmsOrder;
+import com.macro.mall.model.OmsOrderItem;
+import com.macro.mall.model.OmsOrderOperateHistory;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+/**
+ * 订单详情信息
+ */
+public class OmsOrderDetail extends OmsOrder {
+
+    @Getter
+    @Setter
+    private List<OmsOrderItem> orderItemList;
+
+    @Getter
+    @Setter
+    private List<OmsOrderOperateHistory> historyList;
+}
